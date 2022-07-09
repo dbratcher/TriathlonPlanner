9e75fa9b57063531c1b0544d5f86163da4040833
Drew Bratcher <dbratcher@gatech.edu>
2022-07-09 16:42:07 -0400

    Initial Commit

---
+++ Shared/TriathlonPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriathlonPlannerApp.swift
+//  Shared
+//
+//  Created by Drew Bratcher on 7/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TriathlonPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
